7740e9b5a4a30671842cc05308f64d662d8c7f02
mania605 <kim.minsun83@gmail.com>
2024-09-24 10:33:51 +0900

    팝업1일일동안안보기

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -12,6 +12,17 @@ const modal = document.querySelector("aside");
 const btnClose = modal.querySelector("button");
 const ck = modal.querySelector("#ck");
 
+//스크립트 실행되자마자 today=done이라는 쿠키 문자값이 전체 쿠키값에서 있는지 없는지 확인
+const isCookie = document.cookie.indexOf("today=done");
+console.log(isCookie); // -1이면 쿠키없음 그 외의 숫자는 쿠키 있음.
+
+//isCookie값이 음수면 쿠키가 없는것 양수면 쿠키있음
+if (isCookie < 0) {
+	modal.style.display = "block"
+} else {
+	modal.style.display = "none"
+}
+
 btnView.addEventListener("click", () => {
 	console.log(document.cookie);
 });
@@ -26,15 +37,13 @@ btnDel.addEventListener("click", () => {
 
 //닫기 버튼 클릭시 모달 안보임 처리
 btnClose.addEventListener("click", () => {
-<<<<<<< HEAD
+	console.dir(ck); //해당 구문으로 확신시 checked는 프로퍼티가 true면 체크된 상태, false면 체크안된상태
 	//닫기 버튼 클릭시 체크박스가 체크되어 있으면 쿠키생성
 	if (ck.checked) setCoockie("today", "done", 1)
 	modal.style.display = "none";
 });
 
 
-
-=======
 
 //쿠키 생성 함수
 function setCookie(name, value, day) {
@@ -44,3 +53,14 @@ function setCookie(name, value, day) {
 	now.setTime(duedate);
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
 }
+
+//미션 - 웹 페이지 새로 고침시 today=done이라는 쿠키가 있으면 팝업 스타일 none처리해서 숨김
+//없으면 팝업 스타일 block처리해서 보임 처리
+//문자열.indexOf('찾을문자열') 구문으로 특정 쿠키 있는지 없는지 확인 가능
+//step1 - 스크립트 실행되자 마자 document.cookie.indexOf('today=done') 구문을 이용해서 순서값을 반환
+//step2 - 쿠키에 해당하는 문자열이 있으면 0이상의 숫자 반환, -1이라는 음수가 반환
+//step3- 위의 결과값을 조걱식 처림 indexOf() < 0 (쿠키가 없다라는 조건식)
+//step4 - modal.style.display = 'none' 안보이게 처림 (쿠키 있을때)
+//step5 = modal.style.display = 'block' 보임처리 (쿠기 없을떄)
+
+
